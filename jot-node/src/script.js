f480db007626ac3be26fa56f5991fb6059c0cdab
mattcope94 <mattcope94dev@gmail.com>
2020-12-17 17:36:32 -0500

    note script

--- jot-node/src/script.js
+++ jot-node/src/script.js
@@ -1,1 +1,18 @@
 const { PrismaClient } = require("@prisma/client")
+
+const prisma = new PrismaClient()
+
+async function main() {
+    
+    const notes = prisma.note.findMany()
+    console.log(notes)
+  }
+
+main()
+  .catch(e => {
+    throw e
+  })
+ 
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
